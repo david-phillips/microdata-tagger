e7b379f38982f2298922e9e25e2c6d11e29b8333
David Phillips <dave_phillips@elitemail.org>
2012-02-14 21:56:59 -0500

    First cut of working regex matching.
    Behaves as expected on the most simplistic of examples.
    Not currently making any attempt to avoid mismatched tags.

--- microdata-tagger.py
+++ microdata-tagger.py
@@ -6,9 +6,6 @@ try:
 except ImportError:
     import simplejson as json
 
-
-#from BeautifulSoup import BeautifulSoup as Soup
-
 class MDTagger(object):
     ''' '''
     def __init__(self, lexfile, itemref=True):
@@ -30,29 +27,43 @@ class MDTagger(object):
         for semtype in self.lexicon:
             for s in self.lexicon[semtype]:
                 pattern = self.make_pattern(s)
-                self.apply_pattern(pattern)
+                self.apply_pattern(pattern, semtype)
         
-    def apply_pattern(self, p):
+    def apply_pattern(self, p, semtype):
         pattern = re.compile(p, re.I|re.M|re.S)
-        matches = re.findall(pattern, self.markup)
-        print 'Num matches:', len(matches)
-        for match in matches:
-            self.handle_match(match)
+        for match in re.finditer(pattern, self.markup):
+            self.handle_match(match, semtype)
 
     def make_pattern(self, s):
-        ws = r'[ \t\r\n]'
-        tag = r'<[^>]*>'
-        junk = r'(%s|%s)*' % (ws, tag)
-        pattern = junk + junk.join(['('+name+')' for name in s.split()]) + junk  
-        print 'Pattern:', pattern
+        re_junk = r'(<[^>]*>*|[ \t\r\n]*)'
+        re_name_parts = []
+        name_parts = s.split()
+        for np_i in range(len(name_parts)):
+            key = 'name_' + str(np_i)
+            re_part = '(?P<%s>%s)' % (key, name_parts[np_i])
+            re_name_parts.append(re_part)
+        pattern = re_junk + re_junk.join(re_name_parts) + re_junk
         return pattern
 
-    def handle_match(self, match):
-        print 'Match:', match
-        if self.itemref:
-            self.make_itemref_item(match)
-        else:
-            self.make_item(match)
+    def handle_match(self, match, semtype):
+        groups = match.groups()
+        pre_junk = groups[0]
+        post_junk = groups[len(groups)-1]
+        if len(groups) < 3:
+            return
+        # We've processed this already
+        if 'itemscope' in pre_junk:
+            return
+        beg_tag_pos = match.start() + len(pre_junk)
+        end_tag_pos = match.end() - len(post_junk)
+        beg_tag = '<span itemscope itemtype="%s" itemprop="name">' % semtype
+        end_tag = '</span>'
+        self.markup = self.markup[:beg_tag_pos] + beg_tag +\
+                      self.markup[beg_tag_pos:end_tag_pos] + end_tag +\
+                      self.markup[end_tag_pos:]
+        
+        
+        
 
 if __name__ == '__main__':
     tagger = MDTagger('sample-lexicon.json')
