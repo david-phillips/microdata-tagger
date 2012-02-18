9610c1c78a2a73eb734f38aada7ed6bf758fbf9f
David Phillips <dave_phillips@elitemail.org>
2012-02-18 12:14:14 -0500

    Fixed bug in matching code; edited sample files.
    Prior to this commit we were using re.finditer to find
    all matches and then perform subsequent substitutions.
    
    The problem with this is that the start/end properties
    of the match objects are determined before any modifications
    have been made to the underlying text.  Modifying the text
    invalidates all but the first match offsets.
    
    So now we just use re.sub and hand in a callback since
    the substitution str is a function of the match.
    
    Also, set textmode=True default var, which means that by
    default input to the tagger is plain text.

--- microdata-tagger.py
+++ microdata-tagger.py
@@ -6,66 +6,61 @@ try:
 except ImportError:
     import simplejson as json
 
-class MDTagger(object):
+class MicrodataTagger(object):
     ''' '''
-    def __init__(self, lexfile, itemref=True):
-        self.itemref = itemref
+    def __init__(self, lexfile, opt_textmode=True, opt_nameparts=False):
         self.lexfile = lexfile
+        self.opt_textmode = opt_textmode
+        self.opt_nameparts = opt_nameparts
         self.lexicon = None
-        self.markup = None
+        self.old_markup = None
         self.load_lexicon(lexfile)
-        self.itemrefs = []
 
     def load_lexicon(self, lexfile):
         try:
             self.lexicon = json.load(open(lexfile))
-        except JSONDecodeError:
+        except Exception:
             sys.exit('Input lexicon was not valid JSON... exiting.')
         
     def tag_markup(self, markup):
-        self.markup = markup
+        self.old_markup = markup
+        self.new_markup = re.sub(r'<[^>]*>', '', markup)
         for semtype in self.lexicon:
-            for s in self.lexicon[semtype]:
-                pattern = self.make_pattern(s)
+            for lexentry in self.lexicon[semtype]:
+                pattern = self.make_pattern(lexentry)
                 self.apply_pattern(pattern, semtype)
-        
-    def apply_pattern(self, p, semtype):
-        pattern = re.compile(p, re.I|re.M|re.S)
-        for match in re.finditer(pattern, self.markup):
-            self.handle_match(match, semtype)
+        if self.textmode:
+            return tagger.new_markup
+        else:
+            return self.merge_markups()
+
+    def apply_pattern(self, pattern, semtype):
+        def sub_callback(match):
+            groups = match.groups()
+            beg_tag = '<span itemscope itemtype="%s" itemprop="name">' % semtype
+            end_tag = '</span>'
+            return beg_tag + ''.join(groups) + end_tag
+        pattern = re.compile(pattern, re.I|re.M|re.S)
+        self.new_markup = re.sub(pattern, sub_callback, self.new_markup)
 
     def make_pattern(self, s):
-        re_junk = r'(<[^>]*>*|[ \t\r\n]*)'
         re_name_parts = []
         name_parts = s.split()
         for np_i in range(len(name_parts)):
             key = 'name_' + str(np_i)
             re_part = '(?P<%s>%s)' % (key, name_parts[np_i])
             re_name_parts.append(re_part)
-        pattern = re_junk + re_junk.join(re_name_parts) + re_junk
+        if opt_textmode:
+            re_junk = r'([ \t\r\n]*)'
+            pattern = re_junk.join(re_name_parts)
+        else:
+            re_junk = r'(<[^>]*>*|[ \t\r\n]*)'
+            pattern = re_junk + re_junk.join(re_name_parts) + re_junk
         return pattern
 
-    def handle_match(self, match, semtype):
-        groups = match.groups()
-        pre_junk = groups[0]
-        post_junk = groups[len(groups)-1]
-        if len(groups) < 3:
-            return
-        # We've processed this already
-        if 'itemscope' in pre_junk:
-            return
-        beg_tag_pos = match.start() + len(pre_junk)
-        end_tag_pos = match.end() - len(post_junk)
-        beg_tag = '<span itemscope itemtype="%s" itemprop="name">' % semtype
-        end_tag = '</span>'
-        self.markup = self.markup[:beg_tag_pos] + beg_tag +\
-                      self.markup[beg_tag_pos:end_tag_pos] + end_tag +\
-                      self.markup[end_tag_pos:]
-        
-        
-        
-
 if __name__ == '__main__':
-    tagger = MDTagger('sample-lexicon.json')
-    markup = open('sample-markup.html').read()
+    tagger = MicrodataTagger('sample-lexicon.json')
+    tagger.nameparts = True
+    markup = open('sample-text.txt').read()
     tagger.tag_markup(markup)
+    print tagger.new_markup
